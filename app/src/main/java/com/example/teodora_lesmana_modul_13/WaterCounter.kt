0e65a484367b7b6b42838b1e216aa42902b568e5
Michael Leowadu <99025140+MichaelLeowadu@users.noreply.github.com>
2025-06-07 14:59:24 +0700

    Memory in a composable function

--- app/src/main/java/com/example/teodora_lesmana_modul_13/WaterCounter.kt
+++ app/src/main/java/com/example/teodora_lesmana_modul_13/WaterCounter.kt
@@ -5,13 +5,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.remember
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
-        Text("You've had $count glasses.")
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+        val count: MutableState<Int> = remember { mutableStateOf(0) }
+        Text("You've had ${count.value} glasses.")
+        Button(onClick = { count.value++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
         }
     }
