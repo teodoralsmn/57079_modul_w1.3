12ed63b92e5088ab631827dddc323ab2944c7a8c
Michael Leowadu <99025140+MichaelLeowadu@users.noreply.github.com>
2025-06-07 15:34:21 +0700

    Congratulations

---
+++ app/src/main/java/com/example/teodora_lesmana_modul_13/WellnessTask.kt
@@ -0,0 +1,3 @@
+package com.example.teodora_lesmana_modul_13
+
+data class WellnessTask(val id: Int, val label: String)
--- app/src/main/java/com/example/teodora_lesmana_modul_13/WellnessTaskItem.kt
+++ app/src/main/java/com/example/teodora_lesmana_modul_13/WellnessTaskItem.kt
@@ -9,22 +9,28 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Checkbox
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.runtime.remember
+import com.example.teodora_lesmana_modul_13.WellnessTask
 
 @Composable
-fun WellnessTaskItem(
-    taskName: String,
-    onClose: () -> Unit,
-    modifier: Modifier = Modifier
+fun WellnessTasksList(
+    modifier: Modifier = Modifier,
+    list: List<WellnessTask> = remember { getWellnessTasks() }
 ) {
-    Row(
-        modifier = modifier, verticalAlignment = Alignment.CenterVertically
+    LazyColumn(
+        modifier = modifier
     ) {
-        Text(
-            modifier = Modifier.weight(1f).padding(start = 16.dp),
-            text = taskName
-        )
-        IconButton(onClick = onClose) {
-            Icon(Icons.Filled.Close, contentDescription = "Close")
+        items(list) { task ->
+            WellnessTaskItem(taskName = task.label)
         }
     }
 }
