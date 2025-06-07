4538d9b0ccf2feba2787def7eb10af7d9efbafdc
Michael Leowadu <99025140+MichaelLeowadu@users.noreply.github.com>
2025-06-07 15:12:23 +0700

    State Driven UI

--- app/src/main/java/com/example/teodora_lesmana_modul_13/WaterCounter.kt
+++ app/src/main/java/com/example/teodora_lesmana_modul_13/WaterCounter.kt
@@ -9,14 +9,21 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.material3.Button
 import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        val count: MutableState<Int> = remember { mutableStateOf(0) }
-        Text("You've had ${count.value} glasses.")
-        Button(onClick = { count.value++ }, Modifier.padding(top = 8.dp)) {
+        var count by remember { mutableStateOf(0) }
+
+        if (count > 0) {
+            // This text is present if the button has been clicked
+            // at least once; absent otherwise
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
             Text("Add one")
         }
     }
