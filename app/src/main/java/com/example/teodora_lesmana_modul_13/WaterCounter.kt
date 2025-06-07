27a52e32b79119eabdd06eea9b8cf5e2f63be924
Michael Leowadu <99025140+MichaelLeowadu@users.noreply.github.com>
2025-06-07 15:15:28 +0700

    Remember in composition

--- app/src/main/java/com/example/teodora_lesmana_modul_13/WaterCounter.kt
+++ app/src/main/java/com/example/teodora_lesmana_modul_13/WaterCounter.kt
@@ -1,5 +1,6 @@
 package com.example.teodora_lesmana_modul_13
 
+import WellnessTaskItem
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,19 +13,32 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.Row
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
-
         if (count > 0) {
-            // This text is present if the button has been clicked
-            // at least once; absent otherwise
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                    onClose = { showTask = false },
+                    taskName = "Have you taken your 15 minute walk today?"
+                )
+            }
             Text("You've had $count glasses.")
         }
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
-            Text("Add one")
+
+        Row(Modifier.padding(top = 8.dp)) {
+            Button(onClick = { count++ }, enabled = count < 10) {
+                Text("Add one")
+            }
+            Button(
+                onClick = { count = 0 },
+                Modifier.padding(start = 8.dp)) {
+                Text("Clear water count")
+            }
         }
     }
 }
